fc0cc4e984b9b0eb14a8a432b5d6ba72a78f9dab
Albin Forsberg <96afo02@gmail.com>
2025-05-11 15:09:22 +0200

    script to set up symlinks

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOTFILES_DIR="$HOME/projects/dotfiles"
+
+declare -A SYMLINKS=(
+    ["$DOTFILES_DIR/emacs"]="$HOME/.config/emacs"
+)
+
+for src in "${!SYMLINKS[@]}"; do
+    dest="${SYMLINKS[$src]}"
+    echo "Linking $src -> $dest"
+
+    if [ -e  "$dest" ] && [ ! -L "$dest" ]; then
+	mv "$dest" "$dest.backup.$(date +%s)"
+	echo "Backup existing $dest"
+    fi
+
+    ln -sF "$src" "$dest"
+done
+
+echo "All symlinks created."

0b0f82aad3f8653e68d87270ef41482beb95c04b
Albin Forsberg <96afo02@gmail.com>
2025-05-11 18:46:17 +0200

    moved custom-file into separate file

--- emacs/init.el
+++ emacs/init.el
@@ -26,6 +26,9 @@
 (setq auto-save-timeout 20
       auto-save-interval 200)
 
+(setq custom-file (locate-user-emacs-file "custom-vars.el"))
+(load custom-file 'noerror 'nomessage)
+
 ;; C format
 (setq c-default-style "k&r"
       c-basic-offset 4)
@@ -119,20 +122,3 @@
 (keymap-global-set "C-c c" 'org-capture)
 (keymap-global-set "C-x C-b" 'ibuffer)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
- '(gdb-many-windows t)
- '(org-babel-load-languages '((C . t) (emacs-lisp . t)))
- '(package-selected-packages
-   '(nerd-icons-ibuffer nerd-icons-corfu corfu nerd-icons-dired diredfl avy exwm eldoc-box smartparens consult magit orderless vertico projectile which-key yasnippet tree-sitter-langs tree-sitter treesit-auto solarized-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
